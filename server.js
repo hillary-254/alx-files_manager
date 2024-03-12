882f1bdeb119b040e0a0cf565ebe72231b4cc42e
mergik <gmerlix@yahoo.com>
2024-03-12 21:55:13 +0300

    Changed formatting with Prettier

--- server.js
+++ server.js
@@ -1,15 +1,15 @@
-import express from 'express';
-import router from './routes/index';
+import express from "express";
+import router from "./routes/index";
 
 const port = parseInt(process.env.PORT, 10) || 5000;
 
 const app = express();
 
 app.use(express.json());
-app.use('/', router);
+app.use("/", router);
 
 app.listen(port, () => {
-    console.log(`server running on port ${port}`);
+  console.log(`server running on port ${port}`);
 });
 
 export default app;
